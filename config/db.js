14d47e0d63cac0e4b5c0906cb05ab127fd9db62d
Adeeb <adeebanvar66@gmail.com>
2025-02-07 21:01:46 +0530

    timeout

--- config/db.js
+++ config/db.js
@@ -11,12 +11,16 @@ const mysql = require('mysql2');
 //     queueLimit: 0
 // });
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
-    ssl: { rejectUnauthorized: false }  // Important for Aiven SSL connection
-  });
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: { rejectUnauthorized: false }  // Important for Aiven SSL connection
+  , connectTimeout: 30000,           // Increase to 30000 ms (30 seconds)
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
 
 module.exports = pool.promise();
